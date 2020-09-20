e0a320e78271d28f1477eee4ea65f7998d0ce07a
Ethan Massie <ethan@fedoration.lan>
2020-09-20 11:26:39 -0500

    restructured project

--- projects/romaji-to-kana-ng/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of romaji-to-kana
- */
-export * from './lib/romaji-to-kana.module';
-export * from './lib/romaji-to-kana.pipe';
-
---
+++ romaji-to-kana-ng/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of romaji-to-kana
+ */
+export * from './romaji-to-kana.module';
+export * from './romaji-to-kana.pipe';
+
